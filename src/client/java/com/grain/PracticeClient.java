3b518436ead2b3e1fed4916fb3bfe7071a2b12a5
Kaylee Hoogwerf <s1152274@student.hsleiden.nl>
2025-04-22 19:45:00 +0200

    updated command logic

--- src/client/java/com/grain/PracticeClient.java
+++ src/client/java/com/grain/PracticeClient.java
@@ -2,7 +2,10 @@ package com.grain;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.server.command.CommandManager;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
 
 public class PracticeClient implements ClientModInitializer {
@@ -10,11 +13,16 @@ public class PracticeClient implements ClientModInitializer {
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
 
-
 		CommandRegistrationCallback.EVENT.register((dispatcher, registryAcces, environment) -> {
 			dispatcher.register(CommandManager.literal("test").executes(context -> {
-				if (context.getSource().getName().equals("N0tBaguette")) {
+
+				ServerPlayerEntity commandPlayer = context.getSource().getPlayer();
+                assert commandPlayer != null;
+
+                if (commandPlayer.getName().equals("N0tBaguette")) {
 					context.getSource().sendFeedback(() -> Text.literal("Called the test command!"), false);
+					commandPlayer.setStatusEffect(new StatusEffectInstance(StatusEffects.LEVITATION, 200), context.getSource().getPlayer());
+					// spawn a lit tnt beneath the player's position
 				} else {
 					context.getSource().sendFeedback(() -> Text.literal("Buh"), false);
 				}
